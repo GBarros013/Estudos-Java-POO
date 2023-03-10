988328d34e17618376200427e0f9b1a58662e24d
Gabriel <gbarros0495@gmail.com>
2023-03-09 23:21:47 -0300

    Sobrecarga

---
+++ Sobrecarga/src/com/br/dio/sobrecarga/Main.java
@@ -0,0 +1,13 @@
+package com.br.dio.sobrecarga;
+
+public class Main {
+    public static void main(String[] args) {
+        //Quadrilatero
+        System.out.println("Exercício Quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(3, 3);
+        Quadrilatero.area(3, 3, 3);
+    }
+
+
+}
